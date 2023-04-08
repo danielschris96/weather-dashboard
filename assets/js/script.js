66df69e6becd7d1214f079e4402c8ee2b5a4fb5e
unknown <danielschris96@gmail.com>
2023-04-07 18:51:50 -0700

    added event listener and html for widgets

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-console.log("hi")
+var userInput = document.getElementById("text");
+var submitButton = document.getElementById("weather-input");
+
+submitButton.addEventListener("submit", getWeather);
+
+function getWeather(event) {
+    event.preventDefault();
+    var userSearch = document.getElementById("city").value;
+    console.log(userSearch);
+   
+}
+
