f6b0ce483afd9b2685e061bffff9496b580c847e
unknown <danielschris96@gmail.com>
2023-04-07 23:52:53 -0700

    worked on support for 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,12 @@ var currentWind = document.getElementById("current-wind");
 var currentHumidity = document.getElementById("current-humidity");
 var cityList = document.getElementById("city-list");
 var currentIcon = document.getElementById("current-icon");
+var currentDate = document.getElementById("current-date");
+var container2 = document.getElementById("container2");
 
+container2.style.visibility = "hidden";
 
-submitButton.addEventListener("submit", getWeather);
+submitButton.addEventListener("submit", getWeather, getWeather2);
 
 
 
@@ -36,20 +39,25 @@ apiKey = '7b917ea8f42cc643cbf4f7763708048b';
   var wind = data.list[0].wind.speed;
   var humidity = data.list[0].main.humidity;
   var icon = data.list[0].weather[0].icon;
+  var date = data.list[0].dt_txt;
   
   console.log(temp);
   console.log(data.city.name);
+  console.log(date);
   cityName.textContent = data.city.name;
+  currentDate.textContent = date;
   currentTemp.textContent = tempRounded;
   currentWind.textContent = wind;
   currentHumidity.textContent = humidity;
   currentIcon.src = "http://openweathermap.org/img/w/" + icon + ".png";
 
+  container2.style.visibility = "visible";
+
   var saveCity = document.createElement("button");
   saveCity.textContent = data.city.name;
   cityList.appendChild(saveCity);
   saveCity.setAttribute("id", saveCity.value);
-  cityList.addEventListener("click", getWeather2);
+//   cityList.addEventListener("click", getWeather2);
 })
   .catch(error => console.error(error));
   })
@@ -68,31 +76,30 @@ apiKey = '7b917ea8f42cc643cbf4f7763708048b';
   fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + userSearch + '&limit=5&appid=' + apiKey)
   .then(response => response.json())
   .then(data => {
-    // console.log(data[0].lat);
-    var latitude = data[0].lat;
-    var longitude = data[0].lon;
+   for (var i = 1; i < 6; i++) {
+    var latitude = data[i].lat;
+    var longitude = data[i].lon;
     fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey + '&units=imperial')
   .then(response => response.json())
   .then(data => {console.log(data)
-  var temp = data.list[0].main.temp;
-  var tempRounded = temp.toFixed(2);
-  var wind = data.list[0].wind.speed;
-  var humidity = data.list[0].main.humidity;
-  var icon = data.list[0].weather[0].icon;
+  var tempI = data.list[i].main.temp;
+  var tempRoundedI = temp.toFixed(2);
+  var windI = data.list[i].wind.speed;
+  var humidityI = data.list[i].main.humidity;
+  var iconI = data.list[i].weather[i].icon;
   
-  console.log(temp);
+  
+  console.log(tempI);
   console.log(data.city.name);
   cityName.textContent = data.city.name;
-  currentTemp.textContent = tempRounded;
-  currentWind.textContent = wind;
-  currentHumidity.textContent = humidity;
-  currentIcon.src = "http://openweathermap.org/img/w/" + icon + ".png";
 
-  var saveCity = document.createElement("button");
-  saveCity.textContent = data.city.name;
-  cityList.appendChild(saveCity);
-})
-  .catch(error => console.error(error));
-  })
-  .catch(error => console.error(error));
-}
+  currentTemp.textContent = tempRoundedI;
+  currentWind.textContent = windI;
+  currentHumidity.textContent = humidityI;
+  currentIcon.src = "http://openweathermap.org/img/w/" + iconI + ".png";
+
+})}
+//   .catch(error => console.error(error));
+//   })
+//   .catch(error => console.error(error));
+})}
