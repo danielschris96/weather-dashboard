e2f6ffeb9ab484878a223e8b2e90e0bed3a69e09
unknown <danielschris96@gmail.com>
2023-04-08 09:34:20 -0700

    completed 5 day forecast input

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ var container2 = document.getElementById("container2");
 
 container2.style.visibility = "hidden";
 
-submitButton.addEventListener("submit", getWeather, getWeather2);
+submitButton.addEventListener("submit", getWeather);
 
 
 
@@ -58,48 +58,70 @@ apiKey = '7b917ea8f42cc643cbf4f7763708048b';
   cityList.appendChild(saveCity);
   saveCity.setAttribute("id", saveCity.value);
 //   cityList.addEventListener("click", getWeather2);
+
+   for (var i = 7; i < 40; i += 8) {
+    var tempI = data.list[i].main.temp;
+  var tempRoundedI = tempI.toFixed(2);
+  var windI = data.list[i].wind.speed;
+  var humidityI = data.list[i].main.humidity;
+  var iconI = data.list[i].weather[0].icon;
+  var dateI = data.list[i].dt_txt;
+  
+  
+  console.log(tempI);
+  console.log(data.city.name);
+
+ document.getElementById('city-name' + i).textContent = data.city.name;
+ document.getElementById('date' + i).textContent = dateI;
+ document.getElementById('icon' + i).src = "http://openweathermap.org/img/w/" + iconI + ".png";
+ document.getElementById('temp' + i).textContent = tempRoundedI;
+ document.getElementById('wind' + i).textContent = windI;
+ document.getElementById('humidity' + i).textContent = humidityI;
+ console.log(dateI);
+
+}
 })
   .catch(error => console.error(error));
   })
   .catch(error => console.error(error));
 }
  
-function getWeather2(event) {
-    event.preventDefault();
-    console.log(userSearch);
+// function getWeather2(event) {
+//     event.preventDefault();
+//     console.log(userSearch);
    
+//     var userSearch = document.getElementById("city").value;
+
+// apiKey = '7b917ea8f42cc643cbf4f7763708048b';
 
 
-apiKey = '7b917ea8f42cc643cbf4f7763708048b';
-
-
-  fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + userSearch + '&limit=5&appid=' + apiKey)
-  .then(response => response.json())
-  .then(data => {
-   for (var i = 1; i < 6; i++) {
-    var latitude = data[i].lat;
-    var longitude = data[i].lon;
-    fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey + '&units=imperial')
-  .then(response => response.json())
-  .then(data => {console.log(data)
-  var tempI = data.list[i].main.temp;
-  var tempRoundedI = temp.toFixed(2);
-  var windI = data.list[i].wind.speed;
-  var humidityI = data.list[i].main.humidity;
-  var iconI = data.list[i].weather[i].icon;
+//   fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + userSearch + '&limit=5&appid=' + apiKey)
+//   .then(response => response.json())
+//   .then(data => {
+//    for (var i = 1; i < 6; i++) {
+//     var latitude = data[i].lat;
+//     var longitude = data[i].lon;
+//     fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey + '&units=imperial')
+//   .then(response => response.json())
+//   .then(data => {console.log(data)
+//   var tempI = data.list[i].main.temp;
+//   var tempRoundedI = tempI.toFixed(2);
+//   var windI = data.list[i].wind.speed;
+//   var humidityI = data.list[i].main.humidity;
+//   var iconI = data.list[i].weather[i].icon;
   
   
-  console.log(tempI);
-  console.log(data.city.name);
-  cityName.textContent = data.city.name;
+//   console.log(tempI);
+//   console.log(data.city.name);
+//   cityName.textContent = data.city.name;
 
-  currentTemp.textContent = tempRoundedI;
-  currentWind.textContent = windI;
-  currentHumidity.textContent = humidityI;
-  currentIcon.src = "http://openweathermap.org/img/w/" + iconI + ".png";
+//   currentTemp.textContent = tempRoundedI;
+//   currentWind.textContent = windI;
+//   currentHumidity.textContent = humidityI;
+//   currentIcon.src = "http://openweathermap.org/img/w/" + iconI + ".png";
 
-})}
-//   .catch(error => console.error(error));
-//   })
-//   .catch(error => console.error(error));
-})}
+// })}
+// //   .catch(error => console.error(error));
+// //   })
+// //   .catch(error => console.error(error));
+// })}
