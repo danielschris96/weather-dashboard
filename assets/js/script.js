416481082e1b96e7e930790ad376c6005cb33bed
unknown <danielschris96@gmail.com>
2023-04-07 19:42:40 -0700

    added api support for openweather

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,23 @@ function getWeather(event) {
    
 }
 
+apiKey = '7b917ea8f42cc643cbf4f7763708048b';
+
+
+  fetch('http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=' + apiKey)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data[0].lat);
+    var latitude = data[0].lat;
+    var longitude = data[0].lon;
+    fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey)
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+  })
+  .catch(error => console.error(error));
+
+// fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey)
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.error(error));
